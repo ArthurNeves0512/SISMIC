938ed3cc8053e79c8926fd765099d1ae5ea34fce
Arthur Neves <arthurcontroleambiental@gmail.com>
2023-04-12 23:34:38 -0300

    divisao feita

--- Modulo-1/divisao.asm
+++ Modulo-1/divisao.asm
@@ -1,6 +1,19 @@
 
 			.text
-			.def ex3
+			.def divisao
 
-ex3:
-			mov.b #0xA3, R4
+divisao:
+			mov.w 	#0x08, R12
+			mov.w 	#0x02, R13
+			push R4
+			clr R4
+divisaoloop:
+			sub R13,R12
+			inc R4
+			cmp R13,R12
+			jhs divisaoloop
+			mov R4,R13
+			pop R4
+
+			ret
+
